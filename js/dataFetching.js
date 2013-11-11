785aebb42f2fc894e910e2733db6a1cabf540d87
Thuc Hoang <thucth@ifi.uio.no>
2013-11-11 18:40:00 +0100

    Major change
    1. Initiated a joint-project between HKH and NET (DHIS api-files).
    
    2. Changed programs.html to show the survey instead of a table.
    
    3. Cleaned up datafetching.js and also adding optionsets.

--- js/dataFetching.js
+++ js/dataFetching.js
@@ -4,145 +4,146 @@
  * Fetching data from DHIS2 (if localhost it will fetch data from pre-saved json files)
  */
 
-function getUserData() {
-	$('#loginInfo').empty();
-	var userUrl = getHost() + '/api/me.json';
-	console.log("Henter brukerdata fra: " + userUrl);
+ function getUserData() {
+ 	$('#loginInfo').empty();
+ 	var url = getHost() + '/api/me.json';
+ 	console.log("Henter brukerdata fra: " + url);
 
-	$.ajax({
-		url: userUrl,
-		contentType: "application/json",
-		dataType: 'json'
-	}).success(function(data) {
-		var userLoggedIn = 'Currently logged in as ' + data.organisationUnits[0].name + '/<b>' + data.name + '</b> (' + data.email + ')';
-		$('#loginInfo').append(userLoggedIn);
-	}).error(function(data) {
-		console.log("Error...");
-	})
-}
+ 	$.ajax({
+ 		url: url,
+ 		contentType: "application/json",
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		var userLoggedIn = 'Currently logged in as ' + data.organisationUnits[0].name + '/<b>' + data.name + '</b> (' + data.email + ')';
+ 		$('#loginInfo').append(userLoggedIn);
+ 	}).error(function(data) {
+ 		console.log("Error...");
+ 	})
+ }
 
-function getDataSets() {
-	var url = getHost() + '/api/programs.json';
-	console.log("Henter data fra: " + url);
+ function getDataSets() {
+ 	var url = getHost() + '/api/programs.json';
+ 	console.log("Henter data fra: " + url);
 
-	$.ajax({
-		url: url,
-		contentType: "application/json",
-		dataType: 'json'
-	}).success(function(data) {
-		populateProgramList(data);
-	}).error(function(data) {
-		console.log("DATASET ERROR " + data);
-	})
-};
+ 	$.ajax({
+ 		url: url,
+ 		contentType: "application/json",
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		populateProgramList(data);
+ 	}).error(function(data) {
+ 		console.log("DATASET ERROR " + data);
+ 	})
+ };
 
-function populateProgramList(json) {
-	$('#formSelector').empty();
-	var programListFormString = new String();
+ function populateProgramList(json) {
+ 	$('#formSelector').empty();
+ 	var programListFormString = new String();
+ 	if(json.programs.length > 1) {
+ 		programListFormString += '<option value="#">Select one of the programs below</option>'
+ 	}
+ 	$.each(json.programs, function(index, value) {
+ 		if(value.type == 3) {
+ 			programListFormString += '<option value="' + value.id + '">' + value.name + '</option>';
+ 		}
+ 	});
+ 	$('#formSelector').append(programListFormString);
+ }
 
-	$.each(json.programs, function(index, value) {
-		if(value.type == 3) {
-		programListFormString += '<option value="' + value.id + '">' + value.name + '</option>';
-		//programListFormString += '<li><a href="#" data-id="' + value.id + '">' + value.name + '</a></li>';
-		}
-	});
-	$('#formSelector').append(programListFormString);
-}
-	/*
-	$('#dataElementList li a').click(function() {
-		$('#program button').text($(this).text());
-		getProgramStages($(this).text());
-	}); 
-	//$('#programStagesElementList').append('<option value="#">Select one of the stages...</option>');
-}
-*/
-function getProgramStages() {
-	var selectedProgram = document.getElementById("formSelector").selectedIndex;
-	var allPrograms = document.getElementById("formSelector").options;
-	// hente ut id (data)
-	// hent navn til valg, sett til $('#program button').text(name);
-	//var psId = $('#dataElementList li a').attr('data-id');
-	//console.log(psId);
-	var programStageUrl = getHost() + '/api/programs/' + allPrograms[selectedProgram].value + '.json';
-	console.log("Programstage: " + programStageUrl);
+ function getProgramStages() {
+ 	var selectedProgram = document.getElementById("formSelector").selectedIndex;
+ 	var allPrograms = document.getElementById("formSelector").options;
+ 	var url = getHost() + '/api/programs/' + allPrograms[selectedProgram].value + '.json';
+ 	console.log("Programstage: " + url);
 
-	$.ajax({
-		url: programStageUrl,
-		contentType: 'application/json',
-		dataType: 'json'
-	}).success(function(data) {
-		getDataElements(data.programStages[0].id);
-	}).error(function(data) {
-		console.log("PROGRAMSTAGE ERROR");
-	})
-} 
+ 	$.ajax({
+ 		url: url,
+ 		contentType: 'application/json',
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		getDataElements(data.programStages[0].id);
+ 	}).error(function(data) {
+ 		console.log("PROGRAMSTAGE ERROR");
+ 	})
+ } 
 
-function getDataElements(id) {
-	var dataElementsUrl = getHost() + '/api/programStages/' + id + '.json';
-	console.log("Data elements: " + dataElementsUrl);
+ function getDataElements(id) {
+ 	var url = getHost() + '/api/programStages/' + id + '.json';
+ 	console.log("Data elements: " + url);
 
-	$.ajax({
-		url: dataElementsUrl,
-		contentType: 'application/json',
-		dataType: 'json'
-	}).success(function(data) {
-		populateDataElementList(data);
-	}).error(function(data) {
-		console.log("DATAELEMENT ERROR!");
-	})
-}
+ 	$.ajax({
+ 		url: url,
+ 		contentType: 'application/json',
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		$('#formData').empty();
+ 		$.each(data.programStageDataElements, function(index, value) {
+ 			$('#formData').append('<p id="' + value.dataElement.id + '"></p>')
+ 			getDataElementsInfo(value.dataElement.id);
+ 		});
+ 	}).error(function(data) {
+ 		console.log("DATAELEMENT ERROR!");
+ 	})
+ }
 
-function populateDataElementList(json) {
-	$('#dataElementTable').empty();
-	var dataElementListTableString = '<tr><td>Name</td><td>ID</td>';
+ function getDataElementsInfo(id) {
+ 	var url = getHost() + '/api/dataElements/' + id + '.json';
+ 	console.log("DataElements: " + id);
 
-	$.each(json.programStageDataElements, function(index, value) {
-		dataElementListTableString += '<tr><td>' + value.dataElement.name + '</td><td>' + value.dataElement.id + '</td></tr>'; 
-	})
-	$('#dataElementTable').append(dataElementListTableString);
-}
-/*
-function getDataElements(id) {
-	var datael = 'http://apps.dhis2.org/demo/api/dataElements/' + id + '.json';
-	console.log("DataElements: ") + datael;
+ 	$.ajax({
+ 		url: url,
+ 		contentType: 'application/json',
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		checkDataElementOptions(data);
+ 	}).error(function(data) {
+ 		console.log("DataElements ERROR!");
+ 	})
+ }
 
-	$.ajax({
-		url: datael,
-		contentType: 'application/json',
-		dataType: 'json'
-	}).success(function(data) {
-		populateDataElements(data);
-	}).error(function(data) {
-		console.log("DataElements ERROR!");
-	})
-}
+ function checkDataElementOptions(json) {
+ 	if(json.optionSet == null) {
+ 		populateDataElements(json);
+ 	}
+ 	else {
+ 		getOptionSets(json, json.optionSet.id);
+ 	}
+ }
 
-function populateDataElements(json) {
-	console.log(json.name);
-	if(json.optionSet != null) {
-		getOptionSets(json.optionSet.id);
-	}
-}
+ function populateDataElements(json) {
+ 	var dataElementListFormString = new String();
+ 	
+ 	if(json.type == "string" && json.textType == "text") {
+ 		$('#' + json.id).append(json.name + ': <input type="text" name="' + json.name + '"><br />');
+ 	}
+ 	if(json.type == "int" && json.numberType == "int") {
+ 		$('#' + json.id).append(json.name + ': <input type="number" name="' + json.name + '"><br />');
+ 	}
+ 	if(json.type == "date" && json.numberType == "number") {
+ 		$('#' + json.id).append(json.name + ': <input type="date" name="' + json.name + '"><br />');
+ 	}
+ }
 
-function getOptionSets(id) {
-	var optionSetUrl = 'http://apps.dhis2.org/demo/api/optionSets/' + id + '.json';
-	console.log("OptionSet: " + optionSetUrl);
+ function getOptionSets(json, id) {
+ 	var optionSetUrl = getHost() + '/api/optionSets/' + id + '.json';
+ 	console.log(json.name + " optionSet: " + optionSetUrl);
 
-	$.ajax({
-		url: optionSetUrl,
-		contentType: 'application/json',
-		dataType: 'json'
-	}).success(function(data) {
-		populateOptionSetList(data);
-	}).error(function(data) {
-		console.log("OptionSets ERROR!");
-	})
-}
+ 	$.ajax({
+ 		url: optionSetUrl,
+ 		contentType: 'application/json',
+ 		dataType: 'json'
+ 	}).success(function(data) {
+ 		populateOptionSetList(json, data);
+ 	}).error(function(data) {
+ 		console.log("OptionSets ERROR!");
+ 	})
+ }
 
-function populateOptionSetList(json) {
-	if(json.name == "Gender") {
-		for(var i = 0; i < json.options.length; i++) {
-			console.log(i + ": " + json.options[i]);
-		}	
-	}
-}*/
+ function populateOptionSetList(json, data) {
+ 	var optionSetListFromString = json.name + ': <select>';
+ 	for(var i = 0; i < data.options.length; i++) {
+ 		optionSetListFromString += name + ': ' + '<option value="' + i + '">' + data.options[i] + '</option>';
+ 	}
+ 	optionSetListFromString += '</select><br />';
+ 	$('#' + json.id).append(optionSetListFromString);
+ }
