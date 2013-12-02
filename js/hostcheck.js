081eeb354dee62f1f096aef2e849e14e4dda4422
Kennet Vuong <Kennetvuong@gmail.com>
2013-12-03 00:46:36 +0100

    Fixed hostcheck
    Added some logic for getting the right host.
    Returning address based on which host you are on.

--- js/hostcheck.js
+++ js/hostcheck.js
@@ -1,21 +1,27 @@
 /*
- * N.E.T Survey
- * Created by Thuc Hoang and Nguyen Cong Nguyen
- */
+* N.E.T Survey
+* Created by Thuc Hoang and Nguyen Cong Nguyen
+*/
 
 /* 
- * Simple function to check if the host are localhost or the DHIS2 demo.
- */
-function getHost() {
-	var xhReq = new XMLHttpRequest();
-    xhReq.open("GET", "manifest.webapp", false);
-    xhReq.send(null);
-    var serverResponse = JSON.parse(xhReq.responseText);
-    var dhisAPI = serverResponse.activities.dhis.href;
-    if(dhisAPI == "*") {
-    	return "http://localhost/survey";
+* Simple function to check if the host are localhost or the DHIS2 demo.
+*/
+
+function getHost(){
+    var location = window.location;
+    if(location.host === 'localhost'){
+        return "http://localhost/survey";
     }
-    else {
-    	return dhisAPI;
+
+    else if(location.host === 'apps.dhis2.org'){
+        var xhReq = new XMLHttpRequest();
+       xhReq.open("GET", "manifest.webapp", false);
+       xhReq.send(null);
+       var serverResponse = JSON.parse(xhReq.responseText);
+       var dhisAPI = serverResponse.activities.dhis.href;
+       return dhisAPI;
+    }
+    else{
+        return "http://" + location.host + "/survey";
     }
 }
